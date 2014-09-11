7cbd6117f1aeb4a0b5326b3592eb7ac92bf29d0b
Kyösti Herrala <kyosti.herrala@vincit.fi>
2014-09-11 10:17:53 +0300

    Simplify error handling code.

--- pika/adapters/base_connection.py
+++ pika/adapters/base_connection.py
@@ -268,13 +268,13 @@ class BaseConnection(connection.Connection):
             LOGGER.critical("Tried to handle an error where no error existed")
             return
 
-        # Ok errors, just continue what we were doing before
         if error_code in self.ERRORS_TO_IGNORE:
+            # Ok errors, just continue what we were doing before
             LOGGER.debug("Ignoring %s", error_code)
             return
 
-        # Socket is closed, so lets just go to our handle_close method
-        elif error_code in (errno.EBADF, errno.ECONNABORTED):
+        elif error_code in (errno.EBADF, errno.ECONNABORTED, errno.EPIPE):
+            # Socket is closed, so lets just go to our handle_close method
             LOGGER.error("Socket is closed")
 
         elif self.params.ssl and isinstance(error_value, ssl.SSLError):
@@ -288,9 +288,6 @@ class BaseConnection(connection.Connection):
             else:
                 LOGGER.error("SSL Socket error on fd %d: %r",
                              self.socket.fileno(), error_value)
-        elif error_code == errno.EPIPE:
-            # Broken pipe, happens when connection reset
-            LOGGER.error("Socket connection was broken")
         else:
             # Haven't run into this one yet, log it.
             LOGGER.error("Socket Error on fd %d: %s",
